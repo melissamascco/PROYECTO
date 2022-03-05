1b95dab9959ebf76c866332989c95515584b75c4
melissa <meli_m_ch@hotmail.com>
2022-03-04 22:01:10 -0500

    CAMBIO04.03

--- index.js
+++ index.js
@@ -24,30 +24,28 @@ console.log(titulo.innerText);
 let contendor = document.getElementById("subtexto");
 contendor.innerHTML = "<h1>ven y disfruta </h1>";
 
-const contendorproducto = document.getElementById("contenedor-productos");
+const lista = document.querySelector("#contenedor-productos");
 
-mostrarProducto(dataProducto);
+fetch("/data.json") //--------------------------------------- SE UTILIZO ETCH ---------------------------------------------------//
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((producto) => {
+      const div = document.createElement("div");
+      div.className = "producto";
+      div.innerHTML = `
+      <img src=${producto.img}>
+                <h4><class="card-title">${producto.nombre}</h4> 
+                <h6 class="text center  ">$${producto.precio} x KG </h6>
+                <button id="btcomprar${producto.id}"> </button> 
+                <a href="categorias.html" >CATEGORIA</div>
+            `;
 
-function mostrarProducto(array) {
-  array.forEach((producto) => {
-    let div = document.createElement("div");
+      lista.append(div);
 
-    let { img, nombre, precio, id } = producto; //------------DESESTRUCTURACIÓN- OPTIMIZANDO EL PROYECTO------------//
-    div.className = "producto";
-    div.innerHTML = `
-    <img src=${img}>
-    
-    <h4><class="card-title">${nombre}</h4> 
-    <h6 class="text center  ">$${precio} x KG </h6>
-    <button id="btcomprar${id}"> </button> 
-    <a href="categorias.html" >CATEGORIA</div>`;
-
-    contendorproducto.appendChild(div);
-
-    let boton = document.getElementById(`btcomprar${producto.id}`);
-    boton.addEventListener("click", respuesta);
-    function respuesta() {
-      console.log("respuesta1");
-    }
+      let boton = document.getElementById(`btcomprar${producto.id}`);
+      boton.addEventListener("click", respuesta);
+      function respuesta() {
+        console.log("respuesta1");
+      }
+    });
   });
-}
